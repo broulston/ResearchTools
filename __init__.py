fa78c0b198cce4b6aa09e969f66b6dd1f3b3c6a5
broulston <benjamin.roulston@protonmail.com>
2021-03-08 13:55:34 -0500

    initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for LCtools."""
+__author__ = """Benjamin Richard Roulston"""
+__email__ = 'broulston@cfa.harvard.edu'
+__version__ = '0.1.0'
+
+# import everything
+from . import LCtools
+from . import Utils
+from . import mimic_alpha
+from . import PlotTools
+from . import SpecTools
